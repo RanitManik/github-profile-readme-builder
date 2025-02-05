144d8bc50adf29d087d847154cd29c080c93cef0
RanitManik <138437760+RanitManik@users.noreply.github.com>
2025-02-05 14:22:35 +0530

    feat: add SVG assets for profile views and WakaTime; update layout and styles for better readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,17 +11,46 @@ import README from "@/app/README/variant-1";
 
 const Page: NextPage = () => {
     const [markdown, setMarkdown] = useState(README);
+    const [formStage, setFormStage] = useState(1);
 
     return (
         <div className="grid min-h-svh md:grid-cols-5">
             {/* Left: Configuration */}
-            <div className="border-border col-span-2 h-full border-r">
-                <nav className="bg-background_darker border-border flex items-center gap-4 border-b p-4">
+            <div className="border-border col-span-2 flex h-full flex-col border-r">
+                <nav className="bg-background_darker border-border flex h-fit items-center gap-4 border-b p-4">
                     <Logo />
                     <h1 className="overflow-hidden font-semibold whitespace-nowrap select-none">
                         GitHub Profile README Builder
                     </h1>
                 </nav>
+                {formStage === 1 && (
+                    <div className="grid h-full place-content-center gap-4 text-center">
+                        <div>
+                            <h1 className="text-3xl font-bold">Build GitHub README</h1>
+                            <span className="text-sm">
+                                Build the best ever GitHub Profile README
+                            </span>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <label htmlFor="github-username" className="text-sm font-bold">
+                                Enter Your GitHub Username
+                            </label>
+                            <div className="border-border focus-within:outline-accent focus-within:ring-accent flex items-center rounded-md border px-3 py-1.5 transition focus-within:ring-2">
+                                <span className="text-border-400 text-sm">github.com /</span>
+                                <input
+                                    id="github-username"
+                                    autoCapitalize="off"
+                                    autoCorrect="off"
+                                    autoComplete="username"
+                                    autoFocus={true}
+                                    required={true}
+                                    className="caret-accent flex-1 bg-transparent px-1 text-sm focus:outline-none"
+                                    type="text"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
 
             {/* Right: Markdown Preview */}
